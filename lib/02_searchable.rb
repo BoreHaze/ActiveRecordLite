32e6435f9e00798a5faf09dc89c9cc013adb4ea2
Maxfield Lewin <mslewin@gmail.com>
2015-06-12 17:03:39 -0400

    Relation w.i.p.

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,6 +1,5 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
-require_relative 'relation'
 
 module Searchable
   def where(params)
@@ -16,14 +15,14 @@ module Searchable
     #
     # results.map { |sql_obj| self.new(sql_obj) }
 
-    Relation.new(self.class, params)
+    Relation.new(self, params)
 
   end
 end
 
 class Relation
   def initialize(rel_class, initial_condition)
-    @rel_class = rel_class
+    @rel_class = rel_class.to_s.downcase + "s"
     @conditions = [initial_condition]
   end
 
@@ -34,16 +33,45 @@ class Relation
   def build_condition_string
     cond_string = ""
     first = true
-    @conditions.each do  |col, val|
-      first ? cond_string += "WHERE" : cond_string += "AND"
-      cond_string += "#{col} = #{val}"
+    @conditions.each do |cond|
+      col = cond.keys[0]
+      val = cond.values[0]
+
+      first ? cond_string += "WHERE " : cond_string += "AND "
+        if val.is_a?(String)
+          cond_string += "#{col} = '#{val}' "
+        else
+          cond_string += "#{col} = #{val} "
+        end
       first = false
     end
 
     cond_string
   end
 
-  
+  def evaluate
+    results = DBConnection.execute("SELECT * FROM #{@rel_class} #{build_condition_string}")
+    # results.map { |sql_obj| @rel_class.new(sql_obj) }
+    if results.count == 0
+      []
+    elsif results.count == 1
+      @rel_class.new(results[0])
+    else
+      results
+    end
+  end
+
+  def first
+    @rel_class.new(evaluate.first)
+  end
+
+  def length
+    evaluate.length
+  end
+
+  def [](pos)
+    evaluate[pos]
+  end
 end
 
 
